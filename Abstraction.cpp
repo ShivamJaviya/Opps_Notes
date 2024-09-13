ef5ef74e68a85ae66c096ef47c48cb8e12583647
Shivam Javiya <94696278+ShivamJaviya@users.noreply.github.com>
2024-09-13 09:43:53 +0530

    Last Update
    Last Update

--- Abstraction.cpp
+++ Abstraction.cpp
@@ -1,24 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
-class Student{
-     private :
 
-     int a,b;
+class Student {
+    private:
+        int a, b;
 
-     public  :
-      void set(int a,int b){
-          a=a;
-          b=b;
-     }
-     void display(){
-          cout<<a;
-          cout<<b;
-     }
-}
-int main()
-{
+    public:
+        // Function to set values of a and b
+        void set(int a1, int b1) {
+            a = a1;
+            b = b1;
+        }
+
+        // Function to display the values of a and b
+        void display() {
+            cout << "a = " << a << ", b = " << b << endl;
+        }
+};
+
+int main() {
     Student s1;
-    s1.display();
-    s1.set(10,20);
+    s1.set(10, 20);   // Set values first
+    s1.display();     // Now display the values
+
     return 0;
 }
